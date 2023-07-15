1f9832b84cd8a8d21c9eaefbae1a76ab1e3179d4
André Brandão <andrenbrandao@gmail.com>
2023-07-15 12:08:52 -0300

    feat: add health check endpoint to backend

--- cmd/be/main.go
+++ cmd/be/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -36,6 +38,8 @@ func handleConnection(conn net.Conn) {
 	fmt.Fprintf(os.Stdout, "Received request from %s\n", conn.RemoteAddr())
 	reader := bufio.NewReader(conn)
 
+	var path string
+
 	for {
 		s, err := reader.ReadString('\n')
 		if err != nil {
@@ -45,13 +49,36 @@ func handleConnection(conn net.Conn) {
 		if s == "\r\n" {
 			break
 		}
+
+		tokens := strings.Split(s, " ")
+		if tokens[0] == "GET" {
+			path = tokens[1]
+		}
+
 		fmt.Fprint(os.Stdout, s)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
-	conn.Write([]byte("Content-Length: 27\r\n"))
-	conn.Write([]byte("\r\n"))
-	conn.Write([]byte("Hello From Backend Server\r\n"))
-
+	buf := handleRoute(path)
+	conn.Write(buf.Bytes())
 	conn.Close()
+}
+
+func handleRoute(path string) *bytes.Buffer {
+	buf := bytes.Buffer{}
+
+	switch path {
+	case "/health":
+		buf.Write([]byte("HTTP/1.1 204 No Content\r\n"))
+		buf.Write([]byte("\r\n"))
+	case "/":
+		buf.Write([]byte("HTTP/1.1 200 OK\r\n"))
+		buf.Write([]byte("Content-Length: 27\r\n"))
+		buf.Write([]byte("\r\n"))
+		buf.Write([]byte("Hello From Backend Server\r\n"))
+	default:
+		buf.Write([]byte("HTTP/1.1 404 Not Found\r\n"))
+		buf.Write([]byte("\r\n"))
+	}
+
+	return &buf
 }
