f3ad2444cf13f49035b49007091e05c99ffeef23
André Brandão <andrenbrandao@gmail.com>
2023-07-14 22:01:21 -0300

    feat: redirect request from lb to backend

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -2,29 +2,70 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func handleConnection(conn net.Conn) {
 	fmt.Fprintf(os.Stdout, "Received request from %s\n", conn.RemoteAddr())
+	buf := readFromConnection(conn)
+	fmt.Fprint(os.Stdout, buf.String())
+
+	beConn, err := net.Dial("tcp", "127.0.0.1:8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	beConn.Write(buf.Bytes())
+
+	fmt.Print("Response from server: ")
+	buf = readFromConnection(beConn)
+	fmt.Fprint(os.Stdout, buf.String())
+	beConn.Close()
+
+	conn.Close()
+}
+
+func readFromConnection(conn net.Conn) bytes.Buffer {
 	reader := bufio.NewReader(conn)
 
+	buf := bytes.Buffer{}
+	contentLength := 0
 	for {
 		s, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
+		buf.WriteString(s)
+
+		if strings.HasPrefix(s, "Content-Length:") {
+			lengthInStr := strings.Split(s, ":")[1]
+			contentLength, err = strconv.Atoi(strings.TrimSpace(lengthInStr))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 
 		if s == "\r\n" {
 			break
 		}
-		fmt.Fprint(os.Stdout, s)
 	}
 
-	conn.Close()
+	for contentLength != 0 {
+		b, err := reader.ReadByte()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buf.WriteByte(b)
+		contentLength--
+	}
+
+	return buf
 }
 
 func main() {
