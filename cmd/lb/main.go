809dd4955a52424f0b960d076bf2e61f8378d1e0
André Brandão <andrenbrandao@gmail.com>
2023-07-15 12:23:03 -0300

    feat: create health check in load balancer
    - it is still problematic with multithreading since we are changing
    the array and we can get index out of bound errors

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -11,7 +11,28 @@ import (
 	"strings"
 )
 
+/*
+
+How to keep a list of servers and remove them if they are unhealthy?
+
+- Keep a list of the servers we have to connect to and a list
+of the active ones
+- Keep iterating over the configuredServers and checking if they are healthy,
+if one becomes inactive, remove it from the active list
+- If they become active again, add them to the list
+
+What happens if we run this health check in parallel and remove items from the list?
+The main thread may be trying to access the array with the active servers and based
+on the position, it may end up accessing an invalid index, raising a index out of range error.
+
+So, this idea works if we keep executing this in the main thread. But, as soon
+as we start creating goroutines, we get incorrect memory accesses.
+
+How can we solve it?
+*/
+
 var serverAddresses []string = []string{"127.0.0.1:8081", "127.0.0.1:8082", "127.0.0.1:8083"}
+var activeServers []string = serverAddresses
 
 func main() {
 	ln, err := net.Listen("tcp", "127.0.0.1:8080")
@@ -26,8 +47,9 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go handleConnection(conn, serverAddresses[serverPos])
-		serverPos = (serverPos + 1) % len(serverAddresses)
+		go checkHealthyServers()
+		go handleConnection(conn, activeServers[serverPos])
+		serverPos = (serverPos + 1) % len(activeServers)
 		fmt.Println()
 	}
 
@@ -89,4 +111,61 @@ func readFromConnection(conn net.Conn) bytes.Buffer {
 	}
 
 	return buf
+}
+
+func checkHealthyServers() {
+	for _, serverAddress := range serverAddresses {
+		if isHealthy(serverAddress) {
+			activeServers = appendIfMissing(activeServers, serverAddress)
+		} else {
+			activeServers = removeIfPresent(activeServers, serverAddress)
+		}
+	}
+	fmt.Print(activeServers)
+}
+
+func appendIfMissing(slice []string, s string) []string {
+	for _, val := range slice {
+		if val == s {
+			return slice
+		}
+	}
+
+	return append(slice, s)
+}
+
+func removeIfPresent(slice []string, s string) []string {
+	ret := []string{}
+	for _, val := range slice {
+		if val != s {
+			ret = append(ret, val)
+		}
+	}
+
+	return ret
+}
+
+func isHealthy(serverAddress string) bool {
+	beConn, err := net.Dial("tcp", serverAddress)
+	if err != nil {
+		fmt.Printf("Could not connected to server %s", serverAddress)
+		return false
+	}
+
+	buf := bytes.Buffer{}
+	buf.WriteString("GET /health HTTP/1.1\r\n")
+	buf.WriteString("\r\n")
+	beConn.Write(buf.Bytes())
+
+	fmt.Printf("Response from Health Check in server %s: ", serverAddress)
+	buf = readFromConnection(beConn)
+	fmt.Fprint(os.Stdout, buf.String())
+	beConn.Close()
+
+	tokens := strings.Split(buf.String(), " ")
+	if tokens[1] != "200" && tokens[1] != "204" {
+		return false
+	}
+
+	return true
 }
